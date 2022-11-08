1444a3d415298d8e98664535046e0ff952689477
jeshtha24 <97768793+jeshtha24@users.noreply.github.com>
2022-11-08 16:39:27 +0530

    Add files via upload

---
+++ tt.js
@@ -0,0 +1,26 @@
+console.log("hello world") 
+let a= 44
+console.log(a)
+let b= BigInt("566")+BigInt("5")
+console.log(b)
+const item = {
+    "harry":true,
+    "jeshtha":99,
+    "dipesh":undefined,
+}
+console.log(item["jeshtha"])
+let c= "jeshtha"
+let d= 5
+console.log(c+d)
+console.log(typeof(c+d))
+let marks ={
+    harry:90,   
+    jeshth:99,
+    monika:4, 
+}
+for(let i=0;i<Object.keys(marks).length;i++){
+   console.log("the marks of"+Object.keys(marks)[i]+" ") 
+}
+let o= 44
+let i
+
